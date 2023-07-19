a20feb81662a740cc51931a422d5c00aa4f74552
Sergey <shikofreid@yandex.ru>
2023-07-19 23:41:49 +0400

    Added headers

--- frontend/src/utils/Api.js
+++ frontend/src/utils/Api.js
@@ -11,30 +11,38 @@ class Api {
     return Promise.reject(`Error: ${res.status}`);
   }
 
+  _getHeaders() {
+    const token = localStorage.getItem('token');;
+    return {
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${token}`
+    };
+}
+
   addCard(newCardData) {
     return fetch(`${this._url}/cards`, {
       method: "POST",
-      headers: this._headers,
+      headers: this._getHeaders(),
       body: JSON.stringify({ name: newCardData.name, link: newCardData.link }),
     }).then(this._getServerResponse);
   }
 
   getInfoAboutMe() {
     return fetch(`${this._url}/users/me`, {
-      headers: this._headers,
+      headers: this._getHeaders(),
     }).then(this._getServerResponse);
   }
 
   getCards() {
     return fetch(`${this._url}/cards`, {
-      headers: this._headers,
+      headers: this._getHeaders(),
     }).then(this._getServerResponse);
   }
 
   updateProfileData(data) {
     return fetch(`${this._url}/users/me`, {
       method: "PATCH",
-      headers: this._headers,
+      headers: this._getHeaders(),
       body: JSON.stringify({ name: data.name, about: data.about }),
     }).then(this._getServerResponse);
   }
@@ -43,12 +51,12 @@ class Api {
     if (!isLiked) {
       return fetch(`${this._url}/cards/${id}/likes`, {
         method: "PUT",
-        headers: this._headers,
+        headers: this._getHeaders(),
       }).then(this._getServerResponse);
     } else {
       return fetch(`${this._url}/cards/${id}/likes`, {
         method: "DELETE",
-        headers: this._headers,
+        headers: this._getHeaders(),
       }).then(this._getServerResponse);
     }
   }
@@ -56,7 +64,7 @@ class Api {
   updateProfileAvatar(data) {
     return fetch(`${this._url}/users/me/avatar`, {
       method: "PATCH",
-      headers: this._headers,
+      headers: this._getHeaders(),
       body: JSON.stringify({
         avatar: data.avatar
       }),
@@ -66,16 +74,13 @@ class Api {
   deleteCard(cardId) {
     return fetch(`${this._url}/cards/${cardId}`, {
       method: "DELETE",
-      headers: this._headers,
+      headers: this._getHeaders(),
     }).then(this._getServerResponse);
   }
 }
 
 const api = new Api({
-  baseUrl: "https://api.s-al-terentev.nomoredomains.work",
-  headers: {
-    "Content-Type": "application/json",
-  },
+  baseUrl: "https://api.s-al-terentev.nomoredomains.work"
 });
 
 export default api;
