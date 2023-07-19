8c41c6d900574a2cf0a1a400652c6d92a810e6ee
Sergey <shikofreid@yandex.ru>
2023-07-19 23:28:36 +0400

    Replaced auth

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,9 +1,9 @@
 const jwt = require('jsonwebtoken');
 
-const { NODE_ENV = 'production', JWT_SECRET = 'dev-secret' } = process.env;
 const AuthorizationError = require('../errors/AuthorizationError');
 
-const extractBearerToken = (header) => header.replace('Bearer ', '');
+const jwtSecret = process.env.JWT_SECRET;
+const nodeEnv = process.env.NODE_ENV;
 
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
@@ -12,11 +12,11 @@ module.exports = (req, res, next) => {
     return next(new AuthorizationError('Неправильные почта или пароль-1'));
   }
 
-  const token = extractBearerToken(authorization);
+  const token = authorization.replace('Bearer ', '');
   let payload;
 
   try {
-    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
+    payload = jwt.verify(token, nodeEnv === 'production' ? jwtSecret : 'dev-secret');
   } catch (err) {
     return next(new AuthorizationError('Неправильные почта или пароль-2'));
   }
