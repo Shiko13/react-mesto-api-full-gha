988e73a78020b22d52371ab7e91cdd690c933104
Sergey <shikofreid@yandex.ru>
2023-07-21 17:08:01 +0400

    Chanded login

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,7 +1,5 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
-const bcrypt = require('bcryptjs');
-const AuthorizationError = require('../errors/AuthorizationError');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -40,22 +38,22 @@ const userSchema = new mongoose.Schema({
   },
 }, { versionKey: false });
 
-userSchema.statics.findUserByCredentials = function findUserByCredentials(email, password) {
-  return this.findOne({ email }).select('+password')
-    .then((user) => {
-      if (!user) {
-        return Promise.reject(new AuthorizationError('Неправильные почта или пароль'));
-      }
+// userSchema.statics.findUserByCredentials = function findUserByCredentials(email, password) {
+//   return this.findOne({ email }).select('+password')
+//     .then((user) => {
+//       if (!user) {
+//         return Promise.reject(new AuthorizationError('Неправильные почта или пароль'));
+//       }
 
-      return bcrypt.compare(password, user.password)
-        .then((matched) => {
-          if (!matched) {
-            return Promise.reject(new AuthorizationError('Неправильные почта или пароль'));
-          }
+//       return bcrypt.compare(password, user.password)
+//         .then((matched) => {
+//           if (!matched) {
+//             return Promise.reject(new AuthorizationError('Неправильные почта или пароль'));
+//           }
 
-          return user;
-        });
-    });
-};
+//           return user;
+//         });
+//     });
+// };
 
 module.exports = mongoose.model('user', userSchema);
