8e966b8406e3b144ccb106ccc9f973f2a479d054
Sergey <shikofreid@yandex.ru>
2023-07-21 15:22:46 +0400

    Returned old auth.js

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -2,26 +2,25 @@ const jwt = require('jsonwebtoken');
 
 const AuthorizationError = require('../errors/AuthorizationError');
 
-const { JWT_SECRET = 'JWT_SECRET', NODE_ENV = 'production' } = process.env;
+const jwtSecret = process.env.JWT_SECRET;
+const nodeEnv = process.env.NODE_ENV;
 
 module.exports = (req, res, next) => {
-  let token = req.headers.authorization || req.body.token || req.cookies.token;
+  const { authorization } = req.headers;
 
-  if (!token) {
+  if (!authorization || !authorization.startsWith('Bearer ')) {
     return next(new AuthorizationError('Неправильные почта или пароль-1'));
   }
 
-  if (token.startsWith('Bearer ')) {
-    token = token.slice(7);
-  }
+  const token = authorization.replace('Bearer ', '');
+  let payload;
 
   try {
-    const payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
-    req.headers.authorization = token;
-    req.user = payload;
-
-    return next();
-  } catch (error) {
-    return next(new AuthorizationError('Недействительный токен'));
+    payload = jwt.verify(token, nodeEnv === 'production' ? jwtSecret : 'dev-secret');
+  } catch (err) {
+    return next(new AuthorizationError('Неправильные почта или пароль-2'));
   }
+
+  req.user = payload;
+  next();
 };
